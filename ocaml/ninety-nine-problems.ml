a24b8face8daf409e39bb4f7ad65458b77062200
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-01-04 23:01:59 +0000

    OCaml: type annotations for 2 and 3

--- ocaml/ninety-nine-problems.ml
+++ ocaml/ninety-nine-problems.ml
@@ -12,7 +12,8 @@ assert(last [] = None);;
 assert(last [1; 2; 3] = Some 3);;
 
 (* 2. Find the last but one (last and penultimate) elements of a list. (easy) *)
-let rec last_two = function
+let rec last_two: 'a list -> ('a * 'a) option
+  = function
   | [] -> None
   | [a;b] -> Some (a, b)
   | h::t -> last_two t;;
@@ -22,9 +23,11 @@ assert(last_two ["a"] = None);;
 assert(last_two [ "a" ; "b" ; "c" ; "d"  ] = Some ("c", "d"));;
 
 (* Find the k'th element of a list. (easy) *)
-let rec at k = function
-  | [] -> None
-  | h::t -> if k = 1 then Some h else at (k-1) t;;
+let rec at : int -> 'a list -> 'a option
+  = fun k l ->
+    match l with
+    | [] -> None
+    | h::t -> if k = 1 then Some h else at (k-1) t;;
 
 assert(at 3 [ "a"; "b"; "c"; "d"; "e"  ] = Some "c");;
 assert(at 3 [ "a" ] = None);;
