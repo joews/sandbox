ee74f1071e38b85194db334ddd1677bbde69d7c5
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-01-04 22:44:04 +0000

    OCaml 99 problems 1 and 2

---
+++ ocaml/ninety-nine-problems.ml
@@ -0,0 +1,22 @@
+(* my solutions to "99 problems in OCaml"
+ * https://ocaml.org/learn/tutorials/99problems.html
+ *)
+
+(* 1. Write a function last : 'a list -> 'a option that returns the last element of a list. (easy) *)
+let rec last : 'a list -> 'a option = function
+  | [] -> None
+  | [h] -> Some h
+  | h::t -> last t;;
+
+assert(last [] = None);;
+assert(last [1; 2; 3] = Some 3);;
+
+(* 2. Find the last but one (last and penultimate) elements of a list. (easy) *)
+let rec last_two = function
+  | [] -> None
+  | [a;b] -> Some (a, b)
+  | h::t -> last_two t;;
+
+assert(last_two [] = None);;
+assert(last_two ["a"] = None);;
+assert(last_two [ "a" ; "b" ; "c" ; "d"  ] = Some ("c", "d"));;
