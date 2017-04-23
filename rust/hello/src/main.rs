931f5899a35933546fdf68ff9e3b3541f5a345bb
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-04-23 19:33:38 +0100

    Rust: hello world

---
+++ rust/hello/src/main.rs
@@ -0,0 +1,9 @@
+// To build and run by hand:
+// rustc main.rs && ./main
+//
+// To build and run with cargo:
+// cargo run
+fn main() {
+    // println is a macro. Rust macros end with !
+    println!("Hello, world!");
+}
