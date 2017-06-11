db8ed9bba6a45030f6a61d704211fa8ac48fc3f6
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-06-11 22:08:17 +0100

    Unity: roll a ball around on a plane

---
+++ unity/Roll a Ball Tutorial/Assets/scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 delta = new Vector3(h, 0.0f, v);
+		rb.AddForce(delta * speed);
+	}
+}
+ 
