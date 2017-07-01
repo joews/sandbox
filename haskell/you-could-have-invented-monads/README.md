9674182aaa5499985b511a58c3e3a70c7ab414cc
Joe Whitfield-Seed <joe.ws@ripjar.com>
2017-07-01 13:13:12 +0100

    s

--- haskell/you-could-have-invented-monads/README.md
+++ haskell/you-could-have-invented-monads/README.md
@@ -1,3 +1,8 @@
 # You Could Have Invented Monads
 
 http://blog.sigfpe.com/2006/08/you-could-have-invented-monads-and.html
+
+```bash
+cabal update && cabal install hspec
+runhaskell exercises.hs
+```
--- haskell/you-could-have-invented-monads/exercises.hs
+++ haskell/you-could-have-invented-monads/exercises.hs
@@ -1,5 +1,6 @@
--- Exercise 1: define bind to compose two "debuggable" functions Float -> (Float, String)
+import Test.Hspec
 
+-- Exercise 1: define bind to compose two "debuggable" functions Float -> (Float, String)
 bind :: (Float -> (Float,String)) -> ((Float,String) -> (Float,String))
 bind f' (gx, gs) = let (fx, fs) = f' gx in (fx, gs ++ "; " ++ fs)
 
@@ -14,10 +15,16 @@ inc x = (1 + x, "called inc")
 unit :: Float -> (Float, String)
 unit f = (f, "")
 
--- Exercise 3: define lift to create a "debuggable" function
+-- define lift to create a "debuggable" function
 lift :: (Float -> Float) -> (Float -> (Float, String))
 lift f = unit . f
 
-main =
- let (_, composed_debug) = bind times2 (inc 1)
- in putStrLn composed_debug
+-- Exercise 3: show that lift f * lift g = lift (f.g)
+-- a*b is defined to be bind a.b
+-- so:
+-- bind (lift f) . (lift g) = lift (f.g)
+
+main = hspec $ do
+  describe "bind" $ do
+    it "composes 'debuggable' functions" $ do
+      (bind times2 (inc 1)) `shouldBe` (4, "called inc; called times2")
