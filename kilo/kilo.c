781b7346a08def30355fbc233eaccd4e6f664de4
Joe Whitfield-Seed <joe.ws@ripjar.com>
2018-01-28 20:29:01 +0000

    Kilo: handle delete keypress with noop

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -24,6 +24,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   PAGE_UP,
   PAGE_DOWN,
   HOME_KEY,
@@ -158,6 +159,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
